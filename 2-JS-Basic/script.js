c9c198be9801899c717be43c3e6c1abfda97f77d
IanCode09 <ianlombu@protonmail.com>
2020-08-18 22:45:50 +0700

    Adding Variable Mutation and Coercion

--- 2-JS-Basic/script.js
+++ 2-JS-Basic/script.js
@@ -1,3 +1,4 @@
+/*
 // ========== Variable dan Data Types ==========
 
 var firstName = 'Ian';
@@ -12,8 +13,26 @@ console.log(age);
 var fullAge = true;
 console.log(fullAge);
 
-// ========== Variable dan Data Types ========== */
+// ========== Variable dan Data Types ========== 
+*/
 
+/*
+======== VARIABLE MUTATION AND COERCION ========
 
+var firstName = 'Citra Kemurnian'
+var age = 25
 
+console.log('First Name : ' + firstName + ' Umur : ' + age);
+
+var job, isMarried;
+job = 'Web Developer';
+isMarried = false;
+
+console.log(firstName + ' is a ' + job + ' Is he married ? ' + isMarried);
+
+var lastName = prompt('Your Last Name')
+console.log('His name is ' + firstName + ' ' + lastName);
+
+======== VARIABLE MUTATION AND COERCION ========
+*/
 
