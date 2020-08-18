3a2e7bd97cb2555bda0385726f606d34dfec4fc1
IanCode09 <ianlombu@protonmail.com>
2020-08-18 22:21:32 +0700

    Adding Variables and Data Types

---
+++ 2-JS-Basic/script.js
@@ -0,0 +1,19 @@
+// ========== Variable dan Data Types ==========
+
+var firstName = 'Ian';
+console.log(firstName);
+
+var lastName = 'Lombu';
+console.log(lastName);
+
+var age=30
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+
+// ========== Variable dan Data Types ========== */
+
+
+
+
